9ec26fab823bc7f3b1e6fee542bdc5dc8c1d6200
Raj Singh <er.rajsingh@gmail.com>
2026-06-11 01:56:45 -0700

    Update GitHub actions versions

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+<<<<<<< HEAD
     return "Hello Raj SINGH this is CI/CD Pipeline Project Version 2"
+=======
+    return "Hello Raj Singh This is CI/CD Pipeline Project Version 2"
+>>>>>>> 707eebe (Update GitHub actions versions)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
